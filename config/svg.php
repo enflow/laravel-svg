0648195cb359f79aa737662cc1c52ef7d733d290
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2025-09-15 12:22:24 +0200

    Stylesheet config option

--- config/svg.php
+++ config/svg.php
@@ -4,4 +4,5 @@ return [
     'packs' => [
         'custom' => resource_path('img/svgs/'),
     ],
+    'stylesheet' => 'default', // Options are: 'default', 'none' or 'layer-base' for Tailwind 4
 ];
--- resources/views/stylesheet.blade.php
+++ resources/views/stylesheet.blade.php
@@ -1,1 +1,9 @@
-<style id="svg-stylesheet">.svg-auto-size {display: inline-block;font-size: inherit;height: 1em;overflow: visible;vertical-align: -.125em;}</style>
+@if (config('svg.stylesheet') !== 'none')
+<style id="svg-stylesheet">
+    @if (config('svg.stylesheet', 'default') === 'default')
+    .svg-auto-size {display: inline-block;font-size: inherit;height: 1em;overflow: visible;vertical-align: -.125em; }
+    @elseif (config('svg.stylesheet') === 'layer-base')
+    @layer base { .svg-auto-size {display: inline-block;font-size: inherit;height: 1em;overflow: visible;vertical-align: -.125em; } }
+    @endif
+</style>
+@endif
