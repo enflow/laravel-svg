42e45ef8b9aa692dbed935a100f0eed4dbc34226
mbardelmeijer <1583095+mbardelmeijer@users.noreply.github.com>
2026-03-16 14:29:55 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Enflow\Svg\Test;
 
+use Enflow\Svg\SvgServiceProvider;
 use Orchestra\Testbench\TestCase as TestbenchTestCase;
 
 abstract class TestCase extends TestbenchTestCase
@@ -17,7 +18,7 @@ abstract class TestCase extends TestbenchTestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Enflow\Svg\SvgServiceProvider::class,
+            SvgServiceProvider::class,
         ];
     }
 
