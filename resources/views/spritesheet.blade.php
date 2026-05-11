03fd6915228f1eb036b4e75b0154be1e07f42f2e
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2026-05-11 10:42:23 +0200

    Add overflow="visible" attribute to SVG symbol elements in spritesheet

--- resources/views/spritesheet.blade.php
+++ resources/views/spritesheet.blade.php
@@ -1,6 +1,6 @@
 <svg xmlns="http://www.w3.org/2000/svg" id="svg-spritesheet" @if (config('svg.inline_style', true))style="display: none;"@else class="{{ config('svg.hidden_class', 'hidden') }}" hidden @endif>
     @foreach ($spritesheet as $svg)
-        <symbol id="{{ $svg->id() }}" viewBox="{{ implode(' ', $svg->viewBox()) }}">
+        <symbol id="{{ $svg->id() }}" viewBox="{{ implode(' ', $svg->viewBox()) }}" overflow="visible">
             {!! $svg->inner() !!}
         </symbol>
     @endforeach
